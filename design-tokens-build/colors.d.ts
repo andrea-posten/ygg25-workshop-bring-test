c968c8de614e70d68ab41cc490d13529f1a7ff53
Andrea <169157177+andrea-posten@users.noreply.github.com>
2025-03-31 15:06:43 +0200

    css

---
+++ design-tokens-build/colors.d.ts
@@ -0,0 +1,13 @@
+import type {} from '@digdir/designsystemet-react/colors';
+
+declare module '@digdir/designsystemet-react/colors' {
+  export interface MainAndSupportColors {
+    signature-green: never;
+    light-green: never;
+    dark-green: never;
+    lighter-green: never;
+    darker-green: never;
+    grey: never;
+    black: never;
+  }
+}
